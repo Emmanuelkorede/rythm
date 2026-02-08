0787f07622a832e6f4b33a4506e63109b25bbaa9
Emmanuelkorede <emmanueljob2009@gmail.com>
2026-02-08 14:56:27 +0100

    updated the time disply and chnaged the ion for the titile

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -142,7 +142,12 @@ function Todolist() {
 
   function formatDateTime(value) {
     if (!value) return "";
-    return new Date(value).toLocaleString();
+    return new Date(value).toLocaleString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
   }
 
   function toggleTask(index) {
